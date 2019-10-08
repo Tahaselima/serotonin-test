59c8379339ae56ecefbb3f0e164a029286d5c791
edisdev <hedis285@gmail.com>
2019-10-08 11:14:25 +0300

    Add to products page

---
+++ pages/Products.js
@@ -0,0 +1,17 @@
+class Products extends Page {
+  getSorterSelect() { 
+    return $('#sorter') 
+  }
+  getPriceSorterOption()  { 
+    return $('#sorter option[value="price"]')
+  }
+  getStoreAsc() {
+    return $('a[data-role="direction-switcher"]')
+  }
+  getProductTotalCount() {
+    return $('#toolbar-amount > .toolbar-number:last-child')
+  }
+  getAddToCompareButton(index) {
+    return $(`#maincontent > div.columns > div.column.main > div.products.wrapper.grid.products-grid > ol > li:nth-child(${index}) > div > div > div.product-item-inner > div > div.actions-secondary > a.action.tocompare`)
+  }
+}
