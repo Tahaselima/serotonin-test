0137b1c535c92bbdee5a56a1a2f0dfa8493d772a
edisdev <hedis285@gmail.com>
2019-10-08 11:55:19 +0300

    Open browser for homepage

--- pages/Home.js
+++ pages/Home.js
@@ -1,10 +1,10 @@
+import Page from './Page'
 class HomePage extends Page {
-    get gotoProduct(randomId) {
-        // 1 ile 4 arasında -> womentops
-        // 5 ile 6 arasında -> womentbottoms
-        // 19 ile 22 arasında -> mentops
-        // 23 ile 24 arasında -> menButtons
-        return $('#ui-id-1' + randomId);
+    gotoHomePage() {
+      super.open('/')
+    }
+    gotoProduct(randomId) {
+      return $('#ui-id-1' + randomId);
     }
 }
 
