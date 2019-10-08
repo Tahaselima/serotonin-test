646bde006210cb4f80144da6a45080e7a7a7f652
Taha Selim <taksakal@protel.com.tr>
2019-10-08 11:19:42 +0300

    google headless chrome

--- config/wdio.conf.js
+++ config/wdio.conf.js
@@ -56,9 +56,9 @@ exports.config = {
       //
       browserName: 'chrome',
 
-      // 'goog:chromeOptions': {
-      //   args: ['headless', 'disable-gpu'],
-      // },
+      'goog:chromeOptions': {
+        args: ['headless', 'disable-gpu'],
+      },
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
