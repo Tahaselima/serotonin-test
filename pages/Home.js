4d625053ee8bb0dcf969168450c49a98a8d88264
edisdev <hedis285@gmail.com>
2019-10-08 11:30:22 +0300

    add development config

--- pages/Home.js
+++ pages/Home.js
@@ -1,18 +1,11 @@
-class Home extends Page {
-    get goToWomenTops(randomId) { 
-        return $('#ui-id-1' + randomId); // 1 ile 4 arasında
+class HomePage extends Page {
+    get gotoProduct(randomId) {
+        // 1 ile 4 arasında -> womentops
+        // 5 ile 6 arasında -> womentbottoms
+        // 19 ile 22 arasında -> mentops
+        // 23 ile 24 arasında -> menButtons
+        return $('#ui-id-1' + randomId);
     }
-
-    get goToWomenBottoms(randomId) { 
-        return $('#ui-id-1' + randomId); // 5 ile 6 arasında
-    }
-
-    get goToMenTops(randomId) {
-        return $('#ui-id-1' + randomId); // 19 ile 22 arasında
-    }
-
-    get goToMenBottoms(randomId) {
-        return $('#ui-id-1' + randomId); // 23 ile 24 arasında
-    }
-    
 }
+
+export default new HomePage()
